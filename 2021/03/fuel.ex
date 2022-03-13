43f89a51200b8c2ae6f450f26c709aeea9c41170
Tom Karlsson <tomk4@kth.se>
2022-03-13 16:05:57 +0100

    Solution 1

--- 2021/03/fuel.ex
+++ 2021/03/fuel.ex
@@ -14,15 +14,41 @@ defmodule Fuel do
         11001
         00010
         01010
-          """
+        """
+    power(test)
+  end
+
+  def power(input) do
+    String.split(input, "\n")
+    |> Enum.filter(fn(row) -> String.length(row) > 1 end)
+    |> List.foldl(Enum.map(0..4, fn(_) -> 0 end), fn(row, acc) ->
+      count(row,0,acc)
+    end)
+    |> consumption()
 
   end
 
-  def gamma(input) do
-    String.split(input, '\n')
-    |> Stream.map(fn(row) -> String.to_integer(row) end)
-    |> IO.inspect()
+  def consumption(x) do
+    consumption(x, Enum.reverse(Enum.map(0..4, fn(x) ->
+      :math.pow(2,x) end)), {0,0})
+  end
+  def consumption([],[],{g,e}), do: g*e
+  def consumption([hx|tx], [hp|tp], {g,e}) do
+    if hx > 0 do
+      # Gamma found
+      consumption(tx,tp,{g + hp, e})
+    else
+      # Epsilon found
+      consumption(tx,tp,{g,e + hp})
+    end
   end
 
+  def count(_,_,[]), do: []
+  def count(row,n,[h|t]) do
+    case String.slice(row, n..n) do
+      "1" -> [h+1|count(row,n+1,t)]
+      "0" -> [h-1|count(row,n+1,t)]
+    end
+  end
 
 end
